e1bfad1f262c0ab5da1506d643c3e564c71ab058
yokwe <hasegawa.yasuhiro@gmail.com>
2026-02-19 15:40:13 +0900

    Work in progress
    Add copyright comment

--- src/main/java/yokwe/courier/compiler/Compiler.java
+++ src/main/java/yokwe/courier/compiler/Compiler.java
@@ -104,6 +104,7 @@ public class Compiler {
 		var out     = context.outHeader;
 
 		// preamble
+		out.printlnRaw(COPYRIGHT_COMMENT);
 		out.println(
 			"""
 			//
@@ -111,6 +112,8 @@ public class Compiler {
 			//
 
 			#pragma once
+
+			#include "../util/ByteBuffer.h"
 
 			#include "Courier.h"
 			""".formatted(context.name));
@@ -148,11 +151,15 @@ public class Compiler {
 		var out     = context.outSource;
 
 		// preamble
+		out.printlnRaw(COPYRIGHT_COMMENT);
 		out.println(
 			"""
 			//
 			// %s.cpp
 			//
+
+			#include "../util/Util.h"
+			static const Logger logger(__FILE__);
 
 			#include "%s.h"
 			""".formatted(context.name, context.name));
@@ -174,6 +181,39 @@ public class Compiler {
 		// postamble
 		out.println("}");
 	}
+
+	private static final String COPYRIGHT_COMMENT =
+		"""
+		/*******************************************************************************
+		 * Copyright (c) 2026, Yasuhiro Hasegawa
+		 * All rights reserved.
+		 *
+		 * Redistribution and use in source and binary forms, with or without
+		 * modification, are permitted provided that the following conditions are met:
+		 *
+		 * 1. Redistributions of source code must retain the above copyright notice, this
+		 *    list of conditions and the following disclaimer.
+		 *
+		 * 2. Redistributions in binary form must reproduce the above copyright notice,
+		 *    this list of conditions and the following disclaimer in the documentation
+		 *    and/or other materials provided with the distribution.
+		 *
+		 * 3. Neither the name of the copyright holder nor the names of its
+		 *    contributors may be used to endorse or promote products derived from
+		 *    this software without specific prior written permission.
+		 *
+		 * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+		 * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+		 * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+		 * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+		 * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+		 * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+		 * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+		 * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+		 * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+		 * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+		 *******************************************************************************/
+		 """;
 
 	public interface CompilerDecl {
 		public void compileType(Context context, AutoIndentPrintWriter out, String name, Type type);
