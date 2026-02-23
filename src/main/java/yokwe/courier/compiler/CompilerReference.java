656d5ad36634cb79fccf8cd5fa03e3841b6888cd
yokwe <hasegawa.yasuhiro@gmail.com>
2026-02-23 10:42:50 +0900

    Work in progress

--- src/main/java/yokwe/courier/compiler/CompilerReference.java
+++ src/main/java/yokwe/courier/compiler/CompilerReference.java
@@ -34,32 +34,82 @@ import yokwe.courier.compiler.Compiler.CompilerDecl;
 import yokwe.courier.compiler.Compiler.CompilerPair;
 import yokwe.courier.compiler.Compiler.Context;
 import yokwe.courier.program.Cons;
+import yokwe.courier.program.ConsArray;
+import yokwe.courier.program.ConsChoice;
+import yokwe.courier.program.ConsRecord;
+import yokwe.courier.program.ConsReference;
 import yokwe.courier.program.Type;
 import yokwe.util.AutoIndentPrintWriter;
+import yokwe.util.UnexpectedException;
 
 public class CompilerReference extends CompilerPair {
 	private static class CompileHeader implements CompilerDecl {
 		@Override
 		public void compileType(final Context context, final AutoIndentPrintWriter out, final String name, final Type type) {
-//			out.println("// %4d  TYPE  %s  %s", context.decl.line, type.toString(), name); // FIXME
-
+//			out.println("// %4d  TYPE  %s  %s", context.decl.line, type.toString(), name);
 			var ref = type.toTypeReference().toReferenceType().toTYPE();
 			var refName = ref.toQName(context.program.self);
 			out.println("using %s = %s;", name, refName);
 		}
 		@Override
 		public void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final Type type, final Cons cons) {
-			out.println("// %4d  CONS  %s  %s", context.decl.line, type.toString(), name); // FIXME
+			var typeString = type.toTypeReference().ref.toQName(context.program.self);
+
+			switch(cons) {
+			case Cons.Boolean   ut -> compileCons(context, out, name, typeString, ut);
+			case Cons.Number    ut -> compileCons(context, out, name, typeString, ut);
+			case Cons.String    ut -> compileCons(context, out, name, typeString, ut);
+
+			case ConsArray      ut -> compileCons(context, out, name, typeString, ut);
+			case ConsChoice     ut -> compileCons(context, out, name, typeString, ut);
+			case ConsRecord     ut -> compileCons(context, out, name, typeString, ut);
+
+			case ConsReference  ut -> compileCons(context, out, name, typeString, ut);
+
+			default -> throw new UnexpectedException("Unexpected type");
+			}
+		}
+
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final Cons.Boolean cons) {
+			out.println("static inline constexpr %s %s = %d;", typeString, name, cons.value ? 1 : 0);
+		}
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final Cons.Number cons) {
+			out.println("static inline constexpr %s %s = %d;", typeString, name, cons.value);
+		}
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final Cons.String cons) {
+			out.println("static inline constexpr %s %s = %d;", typeString, name, cons.value);
+		}
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final ConsArray cons) {
+//			out.println("// %4d  CONS  ARRAY  %s  %s", context.decl.line, typeString, name, cons.kind);
+			out.println("inline const %s %s {  // %d", typeString, name, cons.constList.size());
+			for(var e: cons.constList) {
+				switch(e) {
+				case Cons.Boolean ut -> out.println("%s,", ut.toString());
+				case Cons.Number  ut -> out.println("%s,", ut.toString());
+				case Cons.String  ut -> out.println("%s,", ut.toString());
+				default -> throw new UnexpectedException("Unexpected type");
+				}
+			}
+			out.println("};");
+		}
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final ConsChoice cons) {
+			out.println("// %4d  CONS  CHOICE %s  %s", context.decl.line, typeString, name, cons.kind); // FIXME
+		}
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final ConsRecord cons) {
+			out.println("// %4d  CONS  RECORD %s  %s", context.decl.line, typeString, name, cons.kind); // FIXME
+		}
+		private void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final String typeString, final ConsReference cons) {
+//			out.println("// %4d  CONS  REF    %s  %s", context.decl.line, typeString, name, cons.kind);
+			out.println("inline const %s %s = %s;", typeString, name, cons.ref.toQName(context.program.self));
 		}
 	}
+
 	private static class CompileSource implements CompilerDecl {
 		@Override
 		public void compileType(final Context context, final AutoIndentPrintWriter out, final String name, final Type type) {
-			// TODO Auto-generated method stub
 		}
 		@Override
 		public void compileCons(final Context context, final AutoIndentPrintWriter out, final String name, final Type type, final Cons cons) {
-			// TODO Auto-generated method stub
 		}
 	}
 
