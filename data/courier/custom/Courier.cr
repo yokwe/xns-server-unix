eff85456923a23438f8ac575a17087421f0ff56b
yokwe <hasegawa.yasuhiro@gmail.com>
2026-02-27 12:26:08 +0900

    Merger Courier and ExpeditedCourier

--- data/courier/custom/Courier.cr
+++ data/courier/custom/Courier.cr
@@ -1,41 +1,51 @@
 Courier: PROGRAM 0 VERSION 3 =
 BEGIN
   -- 4.3.1 Call
-  CallMessageBody: TYPE = RECORD [
+  CallMessage: TYPE = RECORD [
     transactionID: UNSPECIFIED,
     programNumber: LONG CARDINAL,
     versionNumber, procedureValue: CARDINAL];
 --  procedureArguments: RECORD [procedure-dependant]];
     
   -- 4.3.2 Reject
-  ImplimentedVersionNumbers: TYPE = RECORD [
+  VersionRange: TYPE = RECORD [
     lowest, heighest: CARDINAL];
 
-  RejectMessageBody: TYPE = RECORD [
+  RejectMessage: TYPE = RECORD [
     transactionID: UNSPECIFIED,
     rejectDetails: CHOICE OF {
       noSuchProgramNuber(0)  => RECORD [],
-      noSuchVersionNumber(1) => ImplimentedVersionNumbers,
+      noSuchVersionNumber(1) => VersionRange, -- returns supprted version
       noSUchProcedureValue(2),
       invalidArgument(3),
       unspecified(FFFFH)     => RECORD []}];
   
   -- 4.3.3 Return
-  ReturnMessageBody: TYPE = RECORD [
+  ReturnMessage: TYPE = RECORD [
     transacionID: UNSPECIFIED];
 --  procedureResults: RECORD [procedure-depent]];
 
   -- 4.3.4 Abort
-  AbortMessageBody: TYPE = RECORD [
+  AbortMessage: TYPE = RECORD [
     transactionID: UNSPECIFIED,
     errorValue: CARDINAL];
 --  errorArguments: RECORD [error-dependen]];
 
   -- 4.3 Message Type
-  MessageType: TYPE = CHOICE OF {
-    call(0)   => CallMessageBody,
-    reject(1) => RejectMessageBody,
-    return(2) => ReturnMessageBody,
-    abort(3)  => AbortMessageBody};
+  Message: TYPE = CHOICE OF {
+    call(0)   => CallMessage,
+    reject(1) => RejectMessage,
+    return(2) => ReturnMessage,
+    abort(3)  => AbortMessage};
 
+  -- Expedited Message
+  -- NOTE  Expedited Message support only version 3 of Courier
+  ProtocolRange: TYPE = RECORD [
+    lowest, heighest: CARDINAL];
+  HighestProtocol: CARDINAL = 3;
+  LowestProtocol:  CARDINAL = 3;
+
+  ExpeditedMessate: TYPE = RECORD [
+    protocolRange: ProtocolRange,
+    message:       Message];
 END.
