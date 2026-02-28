39b5771ac5d8c31cff21eb3340ce0d21b36bec4a
yokwe <hasegawa.yasuhiro@gmail.com>
2026-02-28 15:01:50 +0900

    Work in progress
    Add ARRAY and SEQUENCE
    Use courier::Courier3

--- src/courier/Courier.h
+++ src/courier/Courier.h
@@ -35,9 +35,9 @@
 
 #pragma once
 
+#include <initializer_list>
 #include <string>
 #include <cstdint>
-#include <variant>
 
 #include "../util/Util.h"
 #include "../util/ByteBuffer.h"
@@ -54,12 +54,135 @@ namespace courier {
 // ARRAY
 //
 // use std::aray for ARRAY
+template<typename T, std::size_t N>
+struct ARRAY {
+private:
+    std::array<T, N> data;
+    std::size_t SIZE = N;
+public:
+    static_assert(N <= 65535UL, "Over size");
 
+    ARRAY() {}
+//    ARRAY(const ARRAY& that) = default;
+    ARRAY(std::initializer_list<T> list) {
+        auto size = list.size();
+        if (size != SIZE) ERROR();
+
+        int i = 0;
+        for(auto& e: list) {
+            data[i++] = e;
+        }
+    }
+
+	T &operator[](std::size_t n) & {
+        return data.at(n);
+    }
+	const T &operator[](std::size_t n) const & {
+        return data.at(n);
+    }
+	T operator[](std::size_t n) const && {
+		return std::move(data.at(n));
+	}
+
+    // read
+    void read(const ByteBuffer& bb) {
+        for(std::size_t i = 0; i < SIZE; i++) {
+            T value;
+            bb.read(value);
+        }
+    }
+    // write
+    void write(ByteBuffer& bb) const {
+        for(std::size_t i = 0; i < SIZE; i++) {
+            T value = data[i];
+            bb.write(value);
+        }
+    }
+    // toString
+    std::string toString() const {
+        std::string ret;
+        ret += std_sprintf("(%d) [", SIZE);
+        for(std::size_t i = 0; i < SIZE; i++) {
+            if (i) ret += "  ";
+            T value = data[i];
+            ret += ::toString(value);
+        }
+        ret += "]";
+        return ret;
+    }
+};
 
 //
 // SEQUENCE
 //
 // use std::vector for SEQUENCE
+template<typename T, std::size_t N=65535>
+struct SEQUENCE {
+private:
+    std::vector<T> data;
+    std::size_t MAX_SIZE = N;
+public:
+    static_assert(N <= 65535UL, "Over limit");
+
+    SEQUENCE() {}
+//    SEQUENCE(const SEQUENCE& that) = default;
+    SEQUENCE(std::initializer_list<T> list) {
+        data.resize(list.size());
+
+        int i = 0;
+        for(auto& e: list) {
+            data[i++] = e;
+        }
+    }
+
+	T &operator[](std::size_t n) & {
+        return data.at(n);
+    }
+	const T &operator[](std::size_t n) const & {
+        return data.at(n);
+    }
+	T operator[](std::size_t n) const && {
+		return std::move(data.at(n));
+	}
+
+    // read
+    void read(const ByteBuffer& bb) {
+        uint16_t size = bb.get16();
+        if (MAX_SIZE < size) ERROR()
+        data.resize(size);
+
+        for(std::size_t i = 0; i < size; i++) {
+            T value;
+            bb.read(value);
+            data[i] = value;
+        }
+    }
+    // write
+    void write(ByteBuffer& bb) const {
+        size_t size = data.size();
+        if (MAX_SIZE <size)  ERROR()
+
+        bb.put16((uint16_t)size);
+        for(std::size_t i = 0; i < size; i++) {
+            T value = data.at(i);
+            bb.write(value);
+        }
+    }
+    // toString
+    std::string toString() const {
+        size_t size = data.size();
+
+        std::string ret;
+        ret += std_sprintf("(%d) [", size);
+        for(std::size_t i = 0; i < size; i++) {
+            if (i) ret += "  ";
+            T value = data[i];
+            ret += ::toString(value);
+        }
+        ret += "]";
+        return ret;
+    }
+};
 
 
 //
@@ -91,387 +214,5 @@ struct ErrorBase {
     virtual void write(ByteBuffer& bb) const = 0;
     virtual std::string toString() const = 0;
 };
-
-
-//
-// Message
-//
-enum class Type : uint16_t {
-    ENUM_NAME_VALUE(Type, CALL,    0)
-    ENUM_NAME_VALUE(Type, REJECT,  1)
-    ENUM_NAME_VALUE(Type, RETURN,  2)
-    ENUM_NAME_VALUE(Type, ABORT,   3)
-};
-std::string toString(Type value);
-
-namespace protocol3{
-//
-struct CallMessage {
-    Type       type;
-    uint16_t   transactionID;
-    uint32_t   programNumber;
-    uint16_t   versionNumber;
-    uint16_t   procedureValue;
-    ByteBuffer arg;
-
-    void read(const ByteBuffer& bb) {
-        bb.read(type, transactionID, programNumber, versionNumber, procedureValue, arg);
-        if (type != Type::CALL) ERROR()
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(type, transactionID, programNumber, versionNumber, procedureValue, arg);
-    }
-    std::string toString() const {
-        return std_sprintf("{%s  %04X  %d  %d  %d  (%d) %s}",
-            courier::toString(type), transactionID, programNumber, versionNumber, procedureValue, arg.byteLimit(), arg.toString());
-    }
-};
-struct ReturnMessage {
-    Type       type;
-    uint16_t   transactionID;
-    ByteBuffer arg;
-
-    void read(const ByteBuffer& bb) {
-        bb.read(type, transactionID, arg);
-        if (type != Type::RETURN) ERROR()
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(type, transactionID, arg);
-    }
-    std::string toString() const {
-        return std_sprintf("{%s  %04X  (%d) %s}",
-            courier::toString(type), transactionID, arg.byteLimit(), arg.toString());
-    }
-};
-struct AbortMessage {
-    Type       type;
-    uint16_t   transactionID;
-    uint16_t   errorValue;
-    ByteBuffer arg;
-
-    void read(const ByteBuffer& bb) {
-        bb.read(type, transactionID, errorValue, arg);
-        if (type != Type::ABORT) ERROR()
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(type, transactionID, errorValue, arg);
-    }
-    std::string toString() const {
-        return std_sprintf("{%s  %04X  (%d) %s}",
-            courier::toString(type), transactionID, arg.byteLimit(), arg.toString());
-    }
-};
-
-struct MessageType {
-    enum class Type : uint16_t {
-        ENUM_NAME_VALUE(Type, CALL,    0)
-        ENUM_NAME_VALUE(Type, REJECT,  1)
-        ENUM_NAME_VALUE(Type, RETURN,  2)
-        ENUM_NAME_VALUE(Type, ABORT,   3)
-    };
-    static std::string toString(Type value);
-
-    struct CallMessage {
-        uint16_t transactionID;
-        uint32_t programNumber;
-        uint16_t versionNumber;
-        uint16_t procedureValue;
-        ByteBuffer arg;
-
-        void read(const ByteBuffer& bb) {
-            bb.read(transactionID, programNumber, versionNumber, procedureValue);
-            arg = bb.rangeRemains();
-        }
-        void write(ByteBuffer& bb) const {
-            bb.write(transactionID, programNumber, versionNumber, procedureValue);
-            bb.write(arg);
-        }
-        std::string toString() const {
-            return std_sprintf("{%04X  %d  %d  %d  (%d) %s}",
-                transactionID, programNumber, versionNumber, procedureValue, arg.byteLimit(), arg.toString());
-        }
-    };
-    struct RejectMessage {
-        enum class Type : uint16_t {
-            ENUM_NAME_VALUE(Type, NO_SUCH_PROGRAM_NUMBER,  0)
-            ENUM_NAME_VALUE(Type, NO_SUCH_VERSION_NUMBER,  1)
-            ENUM_NAME_VALUE(Type, NO_SUCH_PROCEDURE_VALUE, 2)
-            ENUM_NAME_VALUE(Type, INALID_ARGUMENT,         3)
-            ENUM_NAME_VALUE(Type, UNSPECIFIED_ERROR,  0xFFFF)
-        };
-        static std::string toString(Type value);
-
-        struct ImplementedVersionNumbers{
-            uint16_t lowest;
-            uint16_t highest;
-
-            void read(const ByteBuffer& bb) {
-                bb.read(lowest, highest);
-            }
-            void write(ByteBuffer& bb) const {
-                bb.write(lowest, highest);
-            }
-            std::string toString() const {
-                return std_sprintf("{%d %d}", lowest, highest);
-            }
-        };
-
-        Type type;
-        std::variant<std::monostate, ImplementedVersionNumbers> variant;
-
-        RejectMessage() : type(Type::UNSPECIFIED_ERROR) {}
-        RejectMessage(Type type_) : type(type_) {}
-        RejectMessage(const ImplementedVersionNumbers& variant_) : type(Type::NO_SUCH_VERSION_NUMBER), variant(variant_) {}
-
-        bool isNO_SUCH_VERSION_NUMBER() {
-            return type == Type::NO_SUCH_VERSION_NUMBER;
-        }
-        const ImplementedVersionNumbers& toImplementedVersionNumbers() {
-            return std::get<ImplementedVersionNumbers>(variant);
-        }
-
-        void read(const ByteBuffer& bb) {
-            bb.read(type);
-            switch(type) {
-            case Type::NO_SUCH_VERSION_NUMBER:
-            {
-                ImplementedVersionNumbers implementedVersionNumbers;
-                bb.read(implementedVersionNumbers);
-                variant = implementedVersionNumbers;
-            }
-                break;
-            case Type::NO_SUCH_PROGRAM_NUMBER:
-            case Type::NO_SUCH_PROCEDURE_VALUE:
-            case Type::INALID_ARGUMENT:
-            case Type::UNSPECIFIED_ERROR:
-                variant = std::monostate();
-                break;
-            default:
-                ERROR()
-            }
-        }
-        void write(ByteBuffer& bb) const {
-            bb.write(type);
-            switch(type) {
-            case Type::NO_SUCH_VERSION_NUMBER:
-                bb.write(std::get<ImplementedVersionNumbers>(variant));
-                break;
-            case Type::NO_SUCH_PROGRAM_NUMBER:
-            case Type::NO_SUCH_PROCEDURE_VALUE:
-            case Type::INALID_ARGUMENT:
-            case Type::UNSPECIFIED_ERROR:
-                break;
-            default:
-                ERROR()
-            }
-        }
-        std::string toString() const {
-            switch(type) {
-            case Type::NO_SUCH_VERSION_NUMBER:
-            {
-                auto t = std::get<ImplementedVersionNumbers>(variant);
-                return std_sprintf("{%s  %s}", toString(type), t.toString());
-            }
-            case Type::NO_SUCH_PROGRAM_NUMBER:
-            case Type::NO_SUCH_PROCEDURE_VALUE:
-            case Type::INALID_ARGUMENT:
-            case Type::UNSPECIFIED_ERROR:
-                return std_sprintf("{%s}", toString(type));
-            default:
-                ERROR()
-            }
-        }
-    };
-    struct ReturnMessage {
-        uint16_t transactionID;
-        ByteBuffer arg;
-
-        void read(const ByteBuffer& bb) {
-            bb.read(transactionID);
-            arg = bb.rangeRemains();
-        }
-        void write(ByteBuffer& bb) const {
-            bb.write(transactionID);
-            bb.write(arg);
-        }
-        std::string toString() const {
-            return std_sprintf("{%04X  (%d) %s}",
-                transactionID, arg.byteLimit(), arg.toString());
-        }
-    };
-    struct AbortMessage {
-        uint16_t transactionID;
-        uint16_t errorValue;
-        ByteBuffer arg;
-
-        void read(const ByteBuffer& bb) {
-            bb.read(transactionID, errorValue);
-            arg = bb.rangeRemains();
-        }
-        void write(ByteBuffer& bb) const {
-            bb.write(transactionID, errorValue);
-            bb.write(arg);
-        }
-        std::string toString() const {
-            return std_sprintf("{%04X  %d  (%d) %s}",
-                transactionID, errorValue, arg.byteLimit(), arg.toString());
-        }
-    };
-
-    Type type;
-    std::variant<CallMessage, RejectMessage, ReturnMessage, AbortMessage> variant;
-
-    void read(const ByteBuffer& bb) {
-        bb.read(type);
-        switch(type) {
-            case Type::CALL:
-            {
-                CallMessage body;
-                bb.read(body);
-                variant = body;
-            }
-                break;
-            case Type::REJECT:
-            {
-                RejectMessage body;
-                bb.read(body);
-                variant = body;
-            }
-                break;
-            case Type::RETURN:
-            {
-                ReturnMessage body;
-                bb.read(body);
-                variant = body;
-            }
-            break;
-            case Type::ABORT:
-            {
-                AbortMessage body;
-                bb.read(body);
-                variant = body;
-            }
-            break;
-        }
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(type);
-        switch(type) {
-        case Type::CALL:
-        {
-            auto body = std::get<CallMessage>(variant);
-            bb.write(body);
-        }
-            break;
-        case Type::REJECT:
-        {
-            auto body = std::get<RejectMessage>(variant);
-            bb.write(body);
-        }
-            break;
-        case Type::RETURN:
-        {
-            auto body = std::get<ReturnMessage>(variant);
-            bb.write(body);
-        }
-        break;
-        case Type::ABORT:
-        {
-            auto body = std::get<AbortMessage>(variant);
-            bb.write(body);
-        }
-            break;
-        default:
-            ERROR()
-        }
-    }
-};
-
-}
-
-//
-// Protocol
-//
-enum class Protocol : uint16_t {
-    ENUM_NAME_VALUE(Protocol, PROTOCOL_2,    2)
-    ENUM_NAME_VALUE(Protocol, PROTOCOL_3,    3)
-};
-std::string toString(Protocol value);
-
-//
-// ProtocolRange
-//
-struct ProtocolRange {
-    Protocol low;
-    Protocol high;
-
-    ProtocolRange() : low(Protocol::PROTOCOL_3), high(Protocol::PROTOCOL_3) {}
-
-    void read(const ByteBuffer& bb) {
-        bb.read(low, high);
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(low, high);
-    }
-    std::string toString() const {
-        return std_sprintf("{%s  %s}", courier::toString(low), courier::toString(high));
-    }
-};
-
-//
-// VersionRange
-//
-struct VersionRange {
-    uint16_t low;
-    uint16_t high;
-
-    VersionRange() : low(0), high() {}
-
-    void read(const ByteBuffer& bb) {
-        bb.read(low, high);
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(low, high);
-    }
-    std::string toString() const {
-        return std_sprintf("{%d  %d}", low, high);
-    }
-};
-
-//
-// ExpeditedMessage
-//
-namespace expedited {
-//
-struct CallMessage {
-    ProtocolRange          range;
-    protocol3::CallMessage message;
-
-    void read(const ByteBuffer& bb) {
-        bb.read(range, message);
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(range, message);
-    }
-    std::string toString() const {
-        return std_sprintf("{%s  %s}", range.toString(), message.toString());
-    }
-};
-struct ReturnMessage {
-    ProtocolRange            range;
-    protocol3::ReturnMessage message;
-
-    void read(const ByteBuffer& bb) {
-        bb.read(range, message);
-    }
-    void write(ByteBuffer& bb) const {
-        bb.write(range, message);
-    }
-    std::string toString() const {
-        return std_sprintf("{%s  %s}", range.toString(), message.toString());
-    }
-};
-
-}
 
 }
