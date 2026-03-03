3e6f20b371b1641f6f39fa94a8dde046ea584882
yokwe <hasegawa.yasuhiro@gmail.com>
2026-03-03 20:40:26 +0900

    Add ListDomainServed

--- data/courier/private/Clearinghouse3.cr
+++ data/courier/private/Clearinghouse3.cr
@@ -1,5 +1,8 @@
 Clearinghouse: PROGRAM 2 VERSION 3 =
 BEGIN
+	DEPENDS UPON
+		BulkData(0) VERSION 1,
+		Authentication (14) VERSION 3;
 
 Network: TYPE = courier::Network;
 Host:    TYPE = courier::Host;
@@ -12,9 +15,16 @@ NetworkAddress: TYPE = RECORD [
 
 NetworkAddressList: TYPE = SEQUENCE 40 OF NetworkAddress;
 
+Authenticator: TYPE = RECORD [
+	credentials: Authentication.Credentials,
+	verifier: Authentication.Verifier];
+
 
 RetrieveAddresses: PROCEDURE
 RETURNS [address: NetworkAddressList]
 REPORTS [CallError] = 0;
+
+ListDomainServed: PROCEDURE [domains: BulkData.Sink, agent: Authenticator]
+REPORTS [AuthenticationError, CallError] = 1;
 
 END.
