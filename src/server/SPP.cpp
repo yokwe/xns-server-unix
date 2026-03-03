8519c645a75474cbe90f3f7b6189a09c4978eefb
yokwe <hasegawa.yasuhiro@gmail.com>
2026-03-03 20:43:31 +0900

    Temporary commit

--- src/server/SPP.cpp
+++ src/server/SPP.cpp
@@ -30,8 +30,11 @@
 
  
  //
- // pex.cpp
+ // SPP.cpp
  //
+
+#include <mutex>
+#include <vector>
 
 #include "../util/Debug.h"
 #include "../util/Util.h"
@@ -42,18 +45,165 @@ static const Logger logger(__FILE__);
 #include "../xns/SPP.h"
 
 #include "Server.h"
+#include "SPP.h"
 
 namespace server::SPP {
 //
+static uint16_t nextSrcID = 1;
+
+//
+// Sessions
+//
+static Sessions   sessions;
+static std::mutex mutex;
+
+
+void Sessions::maintain() {
+    std::lock_guard<std::mutex>lock (mutex);
+
+    auto now = Util::getSecondsSinceEpoch();
+    auto pred = [&](Session& o) { return o.endingTime < now; };
+    
+   std::erase_if(sessionList, pred);
+}
+Session Sessions::allocate(Context& context, uint16_t dstID) {
+    maintain();
+    std::lock_guard<std::mutex>lock (mutex);
+
+    uint16_t socket = context.allocateSocket();
+    uint16_t srcID = nextSrcID++;
+    Session session{socket, srcID, dstID};
+
+    sessionList.push_back(session);
+    return session;
+}
+void Sessions::free(uint16_t srcID) {
+    std::lock_guard<std::mutex>lock (mutex);
+
+    std::erase_if(sessionList, [&](Session& o){return o.srcID == srcID;});
+}
+bool Sessions::contains(uint16_t srcID) {
+    maintain();
+    for(const auto& e: sessionList) {
+        if (e.srcID == srcID) return true;
+    }
+    return false;
+}
+Session Sessions::get(uint16_t srcID) {
+    maintain();
+    for(const auto& e: sessionList) {
+        if (e.srcID == srcID) return e;
+    }
+    logger.error("Unexpected srcID");
+    logger.error("  srcID  %d", srcID);
+    ERROR()
+}
+
+void Sessions::update(const xns::SPP& spp) {
+    uint16_t srcID = spp.srcID;
+
+    for(auto& e: sessionList) {
+        if (e.srcID == srcID) {
+            e.state      = Session::State::ESTABLISHED;
+            e.endingTime = Util::getSecondsSinceEpoch() + GRACE_PERIOD_EXPIRATION;
+            e.seq        = spp.seq;
+            e.ack        = spp.ack;
+            e.alloc      = spp.alloc;
+            return;
+        }
+    }
+    logger.error("Unexpected value");
+    logger.error("  spp  %d", spp.toString());
+    ERROR()
+}
+
 using SPP   = xns::SPP;
-ByteBuffer process  (ByteBuffer& rx, Context& context) {
-    (void)context;
+
+ByteBuffer processCourierSocket(ByteBuffer& rx, Context& context) {
     SPP rxHeader;
     ByteBuffer rxbb;
     rx.read(rxHeader, rxbb);
     if constexpr (SHOW_PACKET_SPP) logger.info("SPP  >>  %s  (%d) %s", rxHeader.toString(), rxbb.byteLimit(), rxbb.toString());
 
-    return ByteBuffer{};
+    auto txbb = getByteBuffer();
+
+    if (!rxHeader.systemPacket()) {
+        logger.info("Unexpected NOT system packet");
+        return txbb;
+    }
+
+    if (sessions.contains(rxHeader.dstID)) {
+        logger.info("Unexpected srcID in sessions");
+        return txbb;
+    }
+
+    auto session = sessions.allocate(context, rxHeader.srcID);
+    logger.info("NEW SESSION  %d  %04X  %d", session.socket, session.srcID, sessions.sessionList.size());
+    // update IDP dst.socket for new session
+    context.rxHeader->dst.socket = static_cast<xns::Socket>(session.socket);
+
+    SPP txHeader;
+    txHeader.systemPacket(true);
+    txHeader.sendAck(true);
+    txHeader.srcID = session.srcID;
+    txHeader.dstID = session.dstID;
+    txHeader.seq = 0;
+    txHeader.ack = 0;
+    txHeader.alloc = 1;
+
+    txbb.write(txHeader);
+    if constexpr (SHOW_PACKET_SPP) logger.info("SPP  <<  %s", txHeader.toString());
+
+    return txbb;
+}
+
+ByteBuffer processClientSocket(ByteBuffer& rx, Context& context) {
+    SPP rxHeader;
+    ByteBuffer rxbb;
+    rx.read(rxHeader, rxbb);
+    if constexpr (SHOW_PACKET_SPP) logger.info("SPP  >>  %s  (%d) %s", rxHeader.toString(), rxbb.byteLimit(), rxbb.toString());
+
+    auto tx = getByteBuffer();
+
+    if (!sessions.contains(rxHeader.dstID)) {
+        logger.info("Unexpected dstID not in sessions  %04X  %", rxHeader.dstID, sessions.sessionList.size());
+        return tx;
+    }
+
+    auto session = sessions.get(rxHeader.dstID);
+
+    SPP txHeader;
+    txHeader.srcID = session.srcID;
+    txHeader.dstID = session.dstID;
+    txHeader.seq   = session.seq;
+    txHeader.ack   = session.ack;
+    txHeader.alloc = session.alloc;
+
+    if (rxHeader.systemPacket()) {
+        if (rxHeader.sendAck()) {
+            // FIXME
+            txHeader.systemPacket(true);
+            tx.write(txHeader);
+        } else {
+            logger.info("Unexpected system packet");
+            // FIXME
+        }
+    } else {
+        // FIXME
+        auto txbb = callExpeditedMessage(rxbb, context);
+        tx.write(txHeader);
+        tx.write(txbb);
+    }
+
+    return tx;
+}
+
+ByteBuffer process  (ByteBuffer& rx, Context& context) {
+    if (context.rxHeader->dst.socket == xns::Socket::COURIER) {
+        return processCourierSocket(rx, context);
+    } else {
+        return processClientSocket(rx, context);
+    }
 }
 
 }
