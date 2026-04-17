c297a74988824cfa57073ad1cbb725d66087897a
yokwe <hasegawa.yasuhiro@gmail.com>
2026-04-17 12:34:45 +0900

    Change SHOW_PACKET_XXX for terse logging

--- src/util/Debug.h
+++ src/util/Debug.h
@@ -35,14 +35,14 @@
 
 #pragma once
 
-constexpr const int SHOW_PACKET_BOOT     = 1;
+constexpr const int SHOW_PACKET_BOOT     = 0;
 constexpr const int SHOW_PACKET_ECHO     = 0;
 constexpr const int SHOW_PACKET_ERROR    = 1;
 constexpr const int SHOW_PACKET_ETHERNET = 0;
 constexpr const int SHOW_PACKET_IDP      = 0;
-constexpr const int SHOW_PACKET_PEX      = 1;
+constexpr const int SHOW_PACKET_PEX      = 0;
 constexpr const int SHOW_PACKET_RIP      = 0;
-constexpr const int SHOW_PACKET_SPP      = 1;
+constexpr const int SHOW_PACKET_SPP      = 0;
 constexpr const int SHOW_PACKET_TIME     = 0;
 
 constexpr const int SHOW_RESPONSE_DURATION  = 0;
